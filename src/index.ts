d9027c8960eb13cff80c25194397fad5c7052d79
swami11000-maker <swami11000#gmail.com>
2026-06-30 22:57:14 +0530

    go

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import { env } from './config/env.js';
 import { connectDB } from './config/dbCon.js';
 import { app } from './server.js';
 
-const start = async () => {
-	await connectDB();
+// const start = async () => {
+	// await connectDB();
 
 	app.listen(env.PORT, () => {
-		console.log(`Server running at http://localhost:${env.PORT}`);
+		console.log(`Server running at`);
 	});
-};
+// };
 
-start().catch((error) => {
-	console.error('Failed to start server:', error);
-	process.exit(1);
-});
+// start().catch((error) => {
+// 	console.error('Failed to start server:', error);
+// 	process.exit(1);
+// });

71c1f652523ec5544bb7ab16ee0853bce6d848b2
swami11000-maker <swami11000#gmail.com>
2026-06-30 22:33:14 +0530

    env change

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ export const app = express();
 
 app.disable('x-powered-by');
 app.use(helmet());
-app.use(cors({ origin: env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin:'https://project-owphz.vercel.app', credentials: true }));
 app.use(express.json({ limit: '10kb' }));
 
 app.get('/api/health', (_req, res) => {
